b9da3c3388d6df7bc0a6285ad304d3d71200f388
alex <alex.j.hepburn@gmail.com>
2018-10-18 20:22:26 +0100

    Update script to check whole directories

--- bodyscript.py
+++ bodyscript.py
@@ -1,6 +1,14 @@
-tweet_bodys = []
+from os import listdir
+from os.path import isfile, join, exists
 
-with open("data/stocktwits_messages_2018_01_01.json") as f:
-    for line in f:
-        if line.find("body") == 62:
-            tweet_bodys.append(line[68:].split(',"created_at"')[0])
+path = "/media/ntfs/st_data/"
+files = [f for f in listdir(path) if isfile(join(path, f))]
+data = []
+
+for filename in files:
+    with open(path+filename) as inputFile:
+        with open('./data/'+filename, 'w') as outputFile:
+            for line in inputFile:
+                if line.find('"body":"'):
+                    outputFile.write(line[68:].split(',"created_at"')[0]+'\n')
+        
