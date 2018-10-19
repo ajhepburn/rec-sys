7c42faad7f5d79a41501093b56a36d1cf0e5c69d
alex <alex.j.hepburn@gmail.com>
2018-10-19 21:02:23 +0100

    Remove POS tagging, naming entity recognition and parser from pipeline

--- embeddings.py
+++ embeddings.py
@@ -3,10 +3,10 @@ from gensim.models import Word2Vec
 import time
 from datetime import timedelta
 
-sentences = tokens("./data")
+sentences = tokens()
 # train model
 start_time = time.monotonic()
-model = Word2Vec(sentences, min_count=50)
+model = Word2Vec(sentences, min_count=25)
 end_time = time.monotonic()
 print("Time taken to train:",timedelta(seconds=end_time - start_time))
 
--- tokens.py
+++ tokens.py
@@ -3,20 +3,23 @@ from os.path import isfile, join
 import time
 from datetime import timedelta
 import spacy
-nlp = spacy.load('en_core_web_sm')
 
-def tokens(path):
+nlp = spacy.load('en')
+path = "./data/"
+
+def tokens():
     store = []
     files = [f for f in listdir(path) if isfile(join(path, f))]
 
     start_time = time.monotonic()
+#    for filename in nlp.pipe(files, batch_size=10000, n_threads=3):
     for filename in files:
-        with open("./data/"+filename) as f:
+        with open(path+filename) as f:
             #head = [next(f) for x in range(5)]
             for line in f:
-                tokens = nlp(line.lower())
-                tokens = [token.lemma_ for token in tokens if not token.orth_.isspace() and token.is_alpha and not token.is_stop and token.lemma_ != '-PRON-']
-                store.append(tokens)
+                tokens = nlp(line.lower(), disable=['parser', 'tagger', 'ner'])
+                tokens = [token.lemma_ for token in tokens if not token.orth_.isspace() and token.is_alpha and not token.is_stop and token.lemma_ != '-PRON-' and len(token.orth_) != 1]
+                if len(tokens) != 0: store.append(tokens)
 
     end_time = time.monotonic()
     print("Time taken to tokenize:",timedelta(seconds=end_time - start_time))
