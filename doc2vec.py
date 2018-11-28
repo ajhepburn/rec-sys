7a604b0e054eb890fb77ec517b063df2bfbdc31d
alex <alex.j.hepburn@gmail.com>
2018-11-28 23:25:18 +0000

    Add functions for training all documents, averaging user embeddings to file

--- doc2vec.py
+++ doc2vec.py
@@ -1,22 +1,25 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from utils import check_file
+from utils import check_file, NumpyEncoder
 import json
 
 path_data = './data/'
 path_models = './models/'
 
 def get_tagged_data(filename):
-    data = []
+    tagged_data = []
 
     if check_file(path_data, filename):
         with open(path_data+filename) as f:
-            first_entry = json.loads(f.readline())
-            user = list(first_entry.keys())[0]
-            tweets = first_entry[user]
-            for tweet in tweets:
-                data.append(tweet['tokens'])
+            for line in f:
+                entry = json.loads(line)
+                user = list(entry.keys())[0]
+                tweets = entry[user]
+                for tweet in tweets:
+                    # data.append(tweet['tokens'])
+                    td = TaggedDocument(words=tweet['tokens'], tags=[user+"_"+str(tweets.index(tweet))])
+                    tagged_data.append(td)
     
-    tagged_data = [TaggedDocument(words=_d, tags=[user+"_"+str(i)]) for i, _d in enumerate(data)]
+    #tagged_data = [TaggedDocument(words=_d, tags=[user+"_"+str(i)]) for i, _d in enumerate(data)]
     return tagged_data
 
 def train_model(tagged_data):
@@ -59,26 +62,62 @@ def analyse_model(model_name):
 
     #to find vector of doc in training data using tags or in other words, printing the vector of document at index 1 in training data
     #print(model.docvecs['VolumeBot'])
+    # user_docs = [tag for tag in model.docvecs.offset2doctag if tag.startswith('VolumeBot')]
+    # print(user_docs)
+    # for doc_id in user_docs:
+    #     print(model.docvecs[doc_id])
 
-def get_user_embedding(model_name):
-    model = Doc2Vec.load(model_name)
 
-    user_docs = [tag for tag in model.docvecs.offset2doctag if tag.startswith('VolumeBot')]
-    
-    for doc_id in user_docs:
-        if not user_docs.index(doc_id):
-            user_vec = model.docvecs[doc_id]
-        else:
-            user_vec + model.docvecs[doc_id]
-    
-    user_vec /= len(user_docs)
-    return user_vec
+
+def user_embeddings(filename):
+    def get_user_embedding(model, user):
+        user_docs = [tag for tag in model.docvecs.offset2doctag if tag.startswith(user)]
+        
+        for doc_id in user_docs:
+            if not user_docs.index(doc_id):
+                user_vec = model.docvecs[doc_id]
+            else:
+                user_vec + model.docvecs[doc_id]
+        
+        user_vec /= len(user_docs)
+        return user_vec
+
+    def build_embeddings(model_name):
+        model = Doc2Vec.load(model_name)
+        user_embeddings = {}
+
+        if check_file(path_data, filename):
+            with open(path_data+filename) as f:
+                for line in f:
+                    user = list(json.loads(line).keys())[0]
+                    user_vec = get_user_embedding(model, user)
+                    user_embeddings[user] = user_vec
+
+        return user_embeddings
+
+    def write_combined_file(user_embeddings):
+        #c_store = {k: set(v) for k, v in c_store.items()}
+        print("Writing to file...")
+        with open(path_data+filename[:-8]+"_EMBD.txt", 'w') as fp:
+            for k, v in user_embeddings.items():
+                json.dump({k:v}, fp, cls=NumpyEncoder)
+                fp.write("\n")
+        print("Write complete")
+
+    embd_store = build_embeddings(path_models+'d2v.model')
+    write_combined_file(embd_store)
+
+
+
 
 tagged_data = get_tagged_data('st_comb_2018_01_01-2018_01_07_TKN.txt')
+#print(tagged_data)
 #train_model(tagged_data)
 
-#print(tagged_data, end="\n\n")
+# #print(tagged_data, end="\n\n")
 #analyse_model(path_models+'d2v.model')
-print(get_user_embedding(path_models+'d2v.model'))
+user_embeddings('st_comb_2018_01_01-2018_01_07_TKN.txt')
+# user_embeddings = build_embeddings(path_models+'d2v.model')
+#print(get_user_embedding(path_models+'d2v.model'))
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 from pathlib import Path
+import numpy as np
+import json
 
 def check_file(path, filename):
     my_file = Path(path+filename)
     if my_file.is_file(): return 1
     else: return 0
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
