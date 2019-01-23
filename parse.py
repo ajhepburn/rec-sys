350ea7a77c803be98c10f42d8728c7f054e79907
alex <alex.j.hepburn@gmail.com>
2019-01-23 17:40:26 +0000

    Move embeddings, init parser skeleton

---
+++ parse.py
@@ -0,0 +1,18 @@
+class Parser:
+    def __init__(self, path):
+        self.path = path
+
+    def test_db_conn(self):
+        pass
+
+    def extract_features(self):
+        pass
+
+    def tokenize(self):
+        pass
+
+    def build_wlist(self):
+        pass
+
+    def parse(self):
+        pass
