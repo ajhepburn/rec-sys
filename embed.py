d82d5e86e95efca1201dbe524f9fde50926d3952
alex <alex.j.hepburn@gmail.com>
2019-03-04 20:27:24 +0000

    Write simple trainable writer function, finish EmbeddingTrainer class. Closes #13, closes #14.

--- embed.py
+++ embed.py
@@ -6,30 +6,22 @@ from datetime import datetime
 from utils.misc import Utils
 
 class TaggedDocumentIterator(object):
-    def __init__(self, df):
-        self.data = df
+    def __init__(self, trainables_file):
+        self.tf = trainables_file
 
     def __iter__(self):
-        utils = Utils()
-        for _, row in self.data:
-            item_id = row['item_id']
-            tokens = utils.tokenise(row['item_body'])
-            item_sectors = list(map('SECTOR:{0}'.format, row['item_sectors'].split('|') if '|' in row['item_sectors'] else [row['item_sectors']]))
-            item_industries =  list(map('INDUSTRY:{0}'.format, row['item_industries'].split('|') if '|' in row['item_industries'] else [row['item_industries']]))
-            tags = item_id+item_sectors+item_industries
-            yield TaggedDocument(words=tokens, tags=tags)
-            # for line in f:
-            #     s_line = line.strip('\n')
-            #     tokens = s_line.split(" ")
-            #     tag = tokens[0]
-            #     words = tokens[1:]
-            #yield TaggedDocument(words=words, tags=[tag])
+        with open(self.tf) as f:
+            for line in f:
+                words, tags = line.split("\t")
+                yield TaggedDocument(words=words.split(","), tags=tags.split(","))
+
 
 class EmbeddingTrainer:
     def __init__(self):
         self.rpath = './data/csv/metadata_clean.csv'
         self.mpath = './models/doc2vec/'
         self.logpath = './log/d2v/'
+        self.data = self.csv_to_dataframe()
 
     def logger(self):
         """ Sets the logger configuration to report to both std.out and to log to ./log/io/csv/
@@ -55,7 +47,25 @@ class EmbeddingTrainer:
         logger = logging.getLogger()
         data = pd.read_csv(self.rpath, delimiter='\t')
         logger.info("Read file with {0} entries".format(len(data.index)))
-        return data
+        return data[['item_id', 'item_body', 'item_sectors', 'item_industries']]
+
+    def write_trainable_input_file(self):
+        logger, utils = logging.getLogger(), Utils()
+        write_path = './data/trainable/'+str(datetime.now())[:-7]+'.txt'
+        logging.info("Began writing trainables file ({0})".format(write_path))
+        with open(write_path, 'w') as f:
+            for _, row in self.data.iterrows():
+                item_id = row['item_id']
+                tokens = utils.tokenise(row['item_body'])
+                item_sectors = list(map('SECTOR:{0}'.format, row['item_sectors'].split('|') if '|' in row['item_sectors'] else [row['item_sectors']]))
+                item_industries =  list(map('INDUSTRY:{0}'.format, row['item_industries'].split('|') if '|' in row['item_industries'] else [row['item_industries']]))
+
+                words = ",".join([str(x) for x in tokens])
+                tags = ",".join([str(x) for x in [item_id]+item_sectors+item_industries])
+                line = words+"\t"+tags
+                f.write(line+"\n")
+        logging.info("Finished writing trainables file ({0})".format(write_path))
+                
 
     def train_model(self, tagged_docs):
         logger = logging.getLogger()
@@ -68,7 +78,7 @@ class EmbeddingTrainer:
                 epochs=20)
         
         logger.info("Began building vocabulary...")
-        model.build_vocab(tagged_docs, progress_per=250000)
+        model.build_vocab(tagged_docs, progress_per=100000)
         logger.info("Finished building vocabulary.")
 
         logger.info("Began training model...")
@@ -77,11 +87,11 @@ class EmbeddingTrainer:
                         epochs=model.epochs)
         logger.info("Finished training model.")
 
-        model.save(self.mpath+str(datetime.now())[:-7])
+        model.save(self.mpath+str(datetime.now())[:-7]+'.model')
     
     def run(self):
-        data = self.csv_to_dataframe()
-        tagged_docs = TaggedDocumentIterator(data)
+        self.logger()
+        tagged_docs = TaggedDocumentIterator('./data/trainable/2019-03-04 20:14:37.txt')
         self.train_model(tagged_docs)
 
 
