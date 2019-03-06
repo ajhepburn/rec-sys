46c2271a74a13a2c759493763c97084cc0a0a245
alex <alex.j.hepburn@gmail.com>
2019-03-06 15:56:53 +0000

    Split Spotlight ImplicitModel into classes, closes #31

--- model_spotlight.py
+++ model_spotlight.py
@@ -37,31 +37,59 @@ class SpotlightImplicitModel:
         """
 
         df = pd.read_csv(self.rpath, sep='\t')
-        return df
-
-    def run(self):
-        self.logger()
-        df = self.csv_to_df()
         df_interactions = df[['user_id', 'item_tag_ids']]
         df_interactions = df_interactions.groupby(['user_id','item_tag_ids']).size().reset_index() \
                                                .rename(columns={0:'interactions'})
+        return df_interactions
+
+    def build_interactions_object(self, df_interactions: pd.DataFrame) -> Interactions:
+        logger = logging.getLogger()
         user_ids, cashtag_ids = df_interactions['user_id'].values.astype(int), df_interactions['item_tag_ids'].values.astype(int)
         implicit_interactions = Interactions(user_ids, cashtag_ids)
-        train, test = random_train_test_split(implicit_interactions)
+        logger.info("Build interactions object: {}".format(implicit_interactions))
+        return implicit_interactions
+
+    def cross_validation(self, interactions: Interactions) -> tuple:
+        logger = logging.getLogger()
+        train, test = random_train_test_split(interactions)
+        logger.info('Split into \n {} and \n {}.'.format(train, test))
+        return (train, test)
+
+    def fit_implicit_model(self, train: Interactions) -> ImplicitFactorizationModel:
+        logger = logging.getLogger()
+        logger.info("Begin fitting implicit model...")
         implicit_model = ImplicitFactorizationModel(use_cuda=True)
         implicit_model.fit(train, verbose=True)
-        # predictions = implicit_model.predict(406225, item_ids=None)
-        # # print(predictions)
-        train_mrr = mrr_score(implicit_model, train).mean()
-        test_mrr = mrr_score(implicit_model, test).mean()
+        return implicit_model
 
-        train_prec, train_rec = precision_recall_score(implicit_model, train)
-        test_prec, test_rec = precision_recall_score(implicit_model, test)
+    def evaluation(self, model, sets: tuple):
+        logger = logging.getLogger()
+        train, test = sets
 
-        print('Train MRR {:.8f}, test MRR {:.8f}'.format(train_mrr, test_mrr))
-        print('Train Precision@10 {:.8f}, test Precision@10 {:.8f}'.format(train_prec.mean(), test_prec.mean()))
-        print('Train Recall@10 {:.8f}, test Recall@10 {:.8f}'.format(train_rec.mean(), test_rec.mean()))
+        logger.info("Beginning model evaluation...")
+        
+        train_mrr = mrr_score(model, train).mean()
+        test_mrr = mrr_score(model, test).mean()
+        logger.info('Train MRR {:.8f}, test MRR {:.8f}'.format(train_mrr, test_mrr))
+    
 
+        train_prec, train_rec = precision_recall_score(model, train)
+        test_prec, test_rec = precision_recall_score(model, test)
+        logger.info('Train Precision@10 {:.8f}, test Precision@10 {:.8f}'.format(train_prec.mean(), test_prec.mean()))
+        logger.info('Train Recall@10 {:.8f}, test Recall@10 {:.8f}'.format(train_rec.mean(), test_rec.mean()))
+
+    def run(self):
+        self.logger()
+        df_interactions = self.csv_to_df()
+
+        implicit_interactions = self.build_interactions_object(df_interactions)
+        train, test = self.cross_validation(implicit_interactions)
+        
+        implicit_model = self.fit_implicit_model(train)
+ 
+        self.evaluation(implicit_model, (train, test))
+
+        
 if __name__ == "__main__":
     sim = SpotlightImplicitModel()
     sim.run()
