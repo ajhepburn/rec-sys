6201b9455d738824acfce0966b5f0384fd254cbb
alex <alex.j.hepburn@gmail.com>
2019-03-08 19:33:54 +0000

    Add complete docstrings for SpotlightModel, closes #32

--- model_spotlight.py
+++ model_spotlight.py
@@ -1,3 +1,13 @@
+"""Spotlight Model Trainer
+
+This file is responsible for training a number of Spotlight models
+on the StockTwits dataset provided. Contained within are a number of
+models, namely:
+                SpotlightImplicitModel
+                SpotlightSequenceModel
+
+"""
+
 import hashlib
 import logging
 import sys
@@ -17,22 +27,43 @@ from spotlight.evaluation import precision_recall_score, mrr_score
 
 NUM_SAMPLES = 100
 DEFAULT_PARAMS = {
-            'learning_rate': 0.01,
-            'loss': 'pointwise',
-            'batch_size': 256,
-            'embedding_dim': 32,
-            'n_iter': 10,
-            'l2': 0.0
-        }
+    'learning_rate': 0.01,
+    'loss': 'pointwise',
+    'batch_size': 256,
+    'embedding_dim': 32,
+    'n_iter': 10,
+    'l2': 0.0
+}
 
 class Results:
+    """ Responsible for appending model evaluation/hyperparameter information.
+
+    Attributes:
+        _respath (str): The path for storing results files, usually held
+            in the model's log folder.
+        _filename (str): The path to the output results file.
+        _hashlib (md5 hash): Hashes the hyperparameter results for easy parsing..
+
+    """
+
     def __init__(self, filename: str):
         self._respath = './log/models/spotlightimplicitmodel/results/'
         self._filename = filename
-        self._hash = lambda x : hashlib.md5(json.dumps(x, sort_keys=True).encode('utf-8')).hexdigest()
+        self._hash = lambda x: hashlib.md5(
+            json.dumps(x, sort_keys=True).encode('utf-8')
+        ).hexdigest()
         open(self._respath+self._filename, 'a+')
 
     def save(self, evaluation: dict, hyperparameters: dict):
+        """Saves the output from a model evaluation instance to file.
+
+        Args:
+            evaluation (dict): Precision, Recall and Mean Reciprocal
+                Rank metrics from a single model evaluation.
+            hyperparameters (dict): Hyperparameters used by model.
+
+        """
+
         result = {
             'hyperparameters': self._hash(hyperparameters),
             'evaluation': evaluation
@@ -41,6 +72,13 @@ class Results:
             out.write(json.dumps(result) + '\n')
 
     def best(self):
+        """Returns best performing evaluation results and their hyperparameters.
+
+        Returns:
+            dict: Returns best result from current results file.
+
+        """
+
         results = sorted([x for x in self],
                          key=lambda x: -x['test']['mrr'])
 
@@ -74,13 +112,34 @@ class Results:
                 yield datum
 
 
-class SpotlightImplicitModel:
-    def __init__(self, default=False):
-        self.logpath = './log/models/spotlightimplicitmodel/'
-        self.rpath = './data/csv/cashtags_clean.csv'
+class SpotlightModel:
+    """SpotlightModel runs the model training and output.
+
+    This trainer works through its main ``run`` function, which
+    calls other functions within the class.
+
+    An input CSV is read in and split into appropriate Pandas DataFrame objects,
+    extra functionality is included for performance and readability; a hyperparameter
+    sampler and logging function, respectively.
+
+    A Spotlight model must first have an interactions object, mapping entities to
+    internal IDs. These are then split into training/test sets and fed into a number
+    of different kinds of models, from matrix factorization to deep learning architectures.
+    These are then evaluated by a number of metrics and their results are saved via a
+    Results instance, defined above.
+
+    Attributes:
+        _logpath (str): The path for storing model log files.
+        _rpath (str): The path to the input data.
+
+    """
+
+    def __init__(self):
+        self._logpath = './log/models/spotlightimplicitmodel/'
+        self._rpath = './data/csv/cashtags_clean.csv'
 
     def logger(self):
-        """Sets logger config to both std.out and to log ./log/io/csv/cleaner
+        """Sets logger config to both std.out and log ./log/io/csv/spotlightimplicitmodel/
 
         Also sets the formatting instructions for the log file, prints Time,
         Current Thread, Logging Type, Message.
@@ -91,7 +150,7 @@ class SpotlightImplicitModel:
             format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
             handlers=[
                 logging.FileHandler("{0}/{1} ({2}).log".format(
-                    self.logpath,
+                    self._logpath,
                     str(datetime.now())[:-7],
                     'spotlight_implicit_model',
                 )),
@@ -107,7 +166,7 @@ class SpotlightImplicitModel:
 
         """
 
-        df = pd.read_csv(self.rpath, sep='\t')
+        df = pd.read_csv(self._rpath, sep='\t')
         df['count'] = df.groupby(['user_id', 'item_tag_ids']).user_id.transform('size')
         df = df[df['count'] < months*100]
         df_weights = df[['user_id', 'item_tag_ids', 'count']].drop_duplicates(
@@ -128,30 +187,84 @@ class SpotlightImplicitModel:
         listjoin = lambda x: [j for i in x for j in i]
         df['item_timestamp'] = df['item_timestamp'].apply(listjoin)
         df_interactions, df_timestamps = df[['user_id', 'item_tag_ids']], df['item_timestamp']
-        return (df_interactions, df_timestamps, df_weights)
+        return (
+            df_interactions,
+            df_timestamps,
+            df_weights
+        )
 
-    def build_interactions_object(self, df_interactions: pd.DataFrame, df_timestamps: pd.DataFrame, df_weights: pd.DataFrame):
+    def build_interactions_object(self, df_interactions: pd.DataFrame, df_timestamps: pd.DataFrame, df_weights: pd.DataFrame) -> Interactions:
+        """Builds a matrix of interactions between user and cashtag item.
+
+        Takes as params a number of pandas.DataFrame which contains mappings between
+        user-cashtag interactions, associated timestamps, normalised weights for
+        interactions and builds a matrix for input to a Spotlight model.
+
+        Args:
+            df_interactions (pandas.DataFrame): User-Item interactions DataFrame consisting of user and item IDs.
+            df_timestamps (pandas.DataFrame): Timestamps DataFrame consisting of timestamps associated with mappings
+                in df_interactions.
+            df_weights (pandas.DataFrame): Weights DataFrame consisting of weights associated with mappings
+                in df_interactions, that is, the number of times a user has interacted with a particular item.
+
+        Returns:
+            spotlight.interactions.Interactions: Returns Spotlight interactions matrix.
+
+        """
+
         logger = logging.getLogger()
-        user_ids, cashtag_ids = df_interactions['user_id'].values.astype(int), df_interactions['item_tag_ids'].values.astype(int)
+        user_ids = df_interactions['user_id'].values.astype(int)
+        cashtag_ids = df_interactions['item_tag_ids'].values.astype(int)
         timestamps, weights = df_timestamps.values, np.array(df_weights['count'].values)
         normalise = lambda v: v / np.sqrt(np.sum(v**2))
         normalised_weights = normalise(weights)
         interactions = Interactions(
-            user_ids=user_ids, 
-            item_ids=cashtag_ids, 
-            timestamps=np.array([int(x[0]) for x in timestamps]), 
+            user_ids=user_ids,
+            item_ids=cashtag_ids,
+            timestamps=np.array([int(x[0]) for x in timestamps]),
             weights=normalised_weights
         )
         logger.info("Build interactions object: {}".format(interactions))
         return interactions
 
     def cross_validation(self, interactions: Interactions) -> tuple:
+        """Randomly split interactions between training and testing.
+
+        This function takes an interaction set and splits it into two disjoint sets,
+        a training set and a test set.
+
+        Args:
+            interactions (spotlight.interactions.Interactions): Matrix of user-item interactions.
+
+        Returns:
+            tuple: (spotlight.interactions.Interactions, spotlight.interactions.Interactions),
+                A tuple of (train data, test data).
+
+        """
+
         logger = logging.getLogger()
         train, test = random_train_test_split(interactions)
         logger.info('Split into \n {} and \n {}.'.format(train, test))
-        return (train, test)
+        return (
+            train,
+            test
+        )
 
     def sample_implicit_hyperparameters(self, random_state: np.random.RandomState, num: int) -> dict:
+        """Randomly samples hyperparameters for input to model.
+
+        Uses sklearn.model_selection.ParameterSampler to sample random hyperparameters
+        from a given dictionary which are then yielded to a model.
+
+        Args:
+            random_state (np.random.RandomState): Random state to use when fitting.
+            num (int): Number of samples to generate.
+
+        Returns:
+            dict: Returns dict of sampled hyperparameters.
+
+        """
+
         space = {
             'learning_rate': [1e-3, 1e-2, 5 * 1e-2, 1e-1],
             'loss': ['bpr', 'hinge', 'adaptive_hinge', 'pointwise'],
@@ -169,13 +282,26 @@ class SpotlightImplicitModel:
 
         for params in sampler:
             yield params
-        
 
-    def fit_implicit_model(self, train: Interactions, random_state: np.random.RandomState, hyperparameters: dict=None) -> ImplicitFactorizationModel:
+    def fit_implicit_model(self, train: Interactions, random_state: np.random.RandomState, hyperparameters: dict = None) -> ImplicitFactorizationModel:
+        """Trains a Spotlight implicit matrix factorization model.
+
+        Args:
+            train (spotlight.interactions.Interactions): Training set as an interactions matrix.
+            random_state (np.random.RandomState): Random state to use when fitting.
+            hyperparameters (dict, optional): A number of hyperparameters for the model, either sampled
+                from sample_implicit_hyperparameters or default used by model. Defaults can be found
+                in global variable DEFAULT_PARAMS.
+
+        Returns:
+            spotlight.factorization.implicit.ImplicitFactorizationModel: A Spotlight implicit matrix factorization model.
+
+        """
+
         logger = logging.getLogger()
         if hyperparameters:
             logger.info("Beginning fitting implicit model... \n Hyperparameters: \n {0}".format(
-                json.dumps({i:hyperparameters[i] for i in hyperparameters if i!='use_cuda'})
+                json.dumps({i:hyperparameters[i] for i in hyperparameters if i != 'use_cuda'})
             ))
             implicit_model = ImplicitFactorizationModel(
                 loss=hyperparameters['loss'],
@@ -194,6 +320,19 @@ class SpotlightImplicitModel:
         return implicit_model
 
     def evaluation(self, model, sets: tuple):
+        """Evaluates models on a number of metrics
+
+        Takes model and evaluates it by Precision@K/Recall@K, Mean Reciprocal Rank metrics.
+
+        Args:
+            model (Arbitrary): A Spotlight model, can be of different types.
+            sets (tuple): (spotlight.interactions.Interactions, spotlight.interactions.Interactions), A tuple of (train data, test data).
+
+        Returns:
+            dict: A dictionary with all the evaluation metrics.
+
+        """
+
         logger = logging.getLogger()
         train, test = sets
 
@@ -229,7 +368,17 @@ class SpotlightImplicitModel:
             },
         }
 
-    def run(self, results_file: str=None, default: str=False):
+    def run(self, results_file: str = None, default: str = False):
+        """Main function of class which calls on other methods to run model.
+
+        Args:
+            results_file (str, optional): Can be passed in to append to existing
+                results file.
+            default (str, optional): If true, default hyperparameters will be run
+                for each model.
+
+        """
+
         self.logger()
         logger = logging.getLogger()
         random_state = np.random.RandomState(100)
@@ -238,30 +387,34 @@ class SpotlightImplicitModel:
         if not results_file:
             results = Results('{}_results.txt'.format(init_time))
         else:
-            results = Results('{0}results/{1}.txt'.format(self.logpath, results_file))
+            results = Results('{0}results/{1}.txt'.format(self._logpath, results_file))
         best_result = results.best()
 
         df_interactions, df_timestamps, df_weights = self.csv_to_df(months=3)
 
         interactions = self.build_interactions_object(df_interactions, df_timestamps, df_weights)
         train, test = self.cross_validation(interactions)
-        
+
         if not default:
             for hyperparameters in self.sample_implicit_hyperparameters(random_state, NUM_SAMPLES):
                 if hyperparameters in results:
                     continue
 
-                implicit_model = self.fit_implicit_model(hyperparameters=hyperparameters, train=train, random_state=random_state)
+                implicit_model = self.fit_implicit_model(
+                    hyperparameters=hyperparameters,
+                    train=train,
+                    random_state=random_state
+                )
                 evaluation = self.evaluation(implicit_model, (train, test))
                 results.save(evaluation, hyperparameters)
         else:
             implicit_model = self.fit_implicit_model(train=train, random_state=random_state)
             evaluation = self.evaluation(implicit_model, (train, test))
             results.save(DEFAULT_PARAMS, evaluation)
-        
+
         if best_result is not None:
             logger.info('Best result: {}'.format(results.best()))
 
 if __name__ == "__main__":
-    sim = SpotlightImplicitModel()
+    sim = SpotlightModel()
     sim.run(default=True)
