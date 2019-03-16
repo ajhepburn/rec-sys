b40a4884825e81c94b0090da300ed4f5347f0953
alex <alex.j.hepburn@gmail.com>
2019-03-16 01:02:42 +0000

    Write DF to FM converter for LibFM input, closes #75

--- parse.py
+++ parse.py
@@ -1,7 +1,14 @@
-import os, json, sys, logging, csv
 from datetime import datetime
+
+from sklearn.datasets import dump_svmlight_file
+from sklearn.preprocessing import MultiLabelBinarizer
+
+from os.path import join
+
 import pandas as pd
 import numpy as np
+import os, json, sys, logging, csv
+
 
 class AttributeParser:
     def __init__(self, limit: str):
@@ -143,8 +150,112 @@ class CashtagParser:
         df0['item_tag_watchlist_count'] = (df0['item_tag_watchlist_count'] - min(df0['item_tag_watchlist_count']))/(max(df0['item_tag_watchlist_count']) - min(df0['item_tag_watchlist_count']))
         df0.to_csv(self.wpath, sep='\t')
 
+class FMParser:
+    def __init__(self):
+        self._modelpath = './models/xdeepfm/'
+        self._logpath = './log/models/xdeepfm/'
+        self._rpath = './data/csv/cashtags_clean.csv'
+
+    def logger(self):
+            """Sets logger config to both std.out and log ./log/models/smartadaptiverec/
+
+            Also sets the formatting instructions for the log file, prints Time,
+            Current Thread, Logging Type, Message.
+
+            """
+            logging.basicConfig(
+                level=logging.INFO,
+                format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+                handlers=[
+                    logging.FileHandler("{0}/{1} ({2}).log".format(
+                        self._logpath,
+                        str(datetime.now())[:-7],
+                        'smart_adaptive_rec',
+                    )),
+                    logging.StreamHandler(sys.stdout)
+                ])
+
+    def csv_to_df(self, months: int) -> tuple:
+        """Reads in CSV file, converts it to a number of Pandas DataFrames.
+
+        Returns:
+            tuple: Returns tuple of Pandas DataFrames; user features, item features and
+                interactions between items.
+
+        """
+
+        df = pd.read_csv(self._rpath, sep='\t')
+        df['count'] = df.groupby(['user_id', 'item_tag_ids']).user_id.transform('size')
+        df = df[df['count'] < months*100]
+        df_weights = df[['user_id', 'item_tag_ids', 'count']].drop_duplicates(
+            subset=['user_id', 'item_tag_ids']
+        )
+
+        df = df.merge(
+            df.groupby(['user_id', 'item_tag_ids']).item_timestamp.agg(list).reset_index(),
+            on=['user_id', 'item_tag_ids'],
+            how='left',
+            suffixes=['_1', '']
+        ).drop('item_timestamp_1', axis=1)
+
+        df2 = df.groupby(
+            ['user_id', 'item_tag_ids']
+        ).item_timestamp.agg(list).reset_index()
+
+        listjoin = lambda x: [j for i in x for j in i]
+        df2['item_timestamp'] = df2['item_timestamp'].apply(listjoin)
+        df2['item_timestamp'] = df2['item_timestamp'].apply(lambda x: x[0])
+        df2 = df2.sort_values(by=['user_id', 'item_timestamp'])
+        df3 = pd.merge(df2, df_weights, on=["user_id", "item_tag_ids"], how="left")        
+        cols = list(df3.columns)
+        a, b = cols.index('item_timestamp'), cols.index('count')
+        cols[b], cols[a] = cols[a], cols[b]
+        df2 = df3[cols]
+
+        # weights = np.array(df2['count'].values)
+        # normalise = lambda v: v / np.sqrt(np.sum(v**2))
+        # normalised_weights = normalise(weights)
+        # df2['count'] = normalised_weights
+        df2 = df2.rename(columns={'item_tag_ids':'item_id', 'count':'mentions'})
+        df2 = df2.drop(['item_timestamp'], axis=1)
+        df2['user_id'], df2['item_id'] = df2['user_id'].apply(str), df2['item_id'].apply(str)
+        all_items = df2.groupby('user_id',)['item_id'].apply(list)
+        all_counts = df2.groupby('user_id')['mentions'].apply(list)
+        df2['all_item_id'] = dict
+        df2['last_item'] = ''
+
+        last_user_id, last_item_id = None, None
+        for i, row in df2.iterrows():
+            user_id = row['user_id']
+            df2.at[i, 'all_item_id'] = dict(zip(['all_item_id_{0}'.format(i) for i in all_items[user_id]], all_counts[user_id]))
+            if user_id != last_user_id:
+                last_user_id, last_item_id = user_id, row['item_id']
+                continue
+            df2.at[i, 'last_item'], last_item_id = 'last_item_id_'+last_item_id, row['item_id']
+        x, y = df2[['user_id', 'item_id', 'all_item_id', 'last_item']].copy(), df2['mentions'].copy()
+        return (x, y)
+
+    def df_to_libsvm(self, x: pd.DataFrame, y: pd.DataFrame):
+        all_items, last_item = x.pop('all_item_id'), pd.get_dummies(x.pop('last_item'))
+        x = pd.get_dummies(x)
+        df_all_item_id = all_items.apply(pd.Series)
+        df_all_item_id.fillna(value=0, inplace=True)
+        df_all_item_id = df_all_item_id.div(df_all_item_id.sum(axis=1), axis=0)
+        df_all_item_id = df_all_item_id.astype(np.float32)
+        x = x.join(df_all_item_id, how='outer')
+        x = x.join(last_item, how='outer')
+        mat = x.as_matrix()
+        dump_svmlight_file(mat, y, join(self._modelpath, 'svm-output.libsvm'))
+
+    def run(self):
+        x, y = self.csv_to_df(months=3)
+        self.df_to_libsvm(x, y)
+        
+
 if __name__ == "__main__":
     # ab = AttributeParser('2017_04_01') # 2017_02_01
     # ab.run()
-    ctp = CashtagParser()
-    ctp.conversion_to_cashtag_orientation()
+    # ctp = CashtagParser()
+    # ctp.conversion_to_cashtag_orientation()
+    dfm = xDeepFMModel()
+    dfm.run()
