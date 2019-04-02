e5f427c858f0ecb4c8eaf7850dc76bf87cf7811d
alex <alex.j.hepburn@gmail.com>
2019-04-02 18:48:50 +0100

    Add byTitle queryer and Levenshtein algorithm to Queryer

--- query.py
+++ query.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-from utils.queries import QUERIES
+from utils.queries import queryFormatter
+from Levenshtein import _levenshtein
 
 import pandas as pd
 import more_itertools as mit
@@ -16,6 +17,7 @@ class Queryer:
     def __init__(self):
         self._logpath = './log/database/dbquery'
         self._rpath = './data/csv/'
+        self._wpath = './data/txt/queryer/levenshtein/'
 
     def logger(self):
         """Sets logger config to both std.out and log ./log/io/dbquery
@@ -36,25 +38,9 @@ class Queryer:
                 logging.StreamHandler(sys.stdout)
             ])
 
-    def run_query(self, symbols: str, query: str):
+    def run_query(self, symbols: str, keyword: str):
         sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-        # query = """
-        #     PREFIX dbp: <http://dbpedia.org/property/>
-        #     PREFIX dbo: <http://dbpedia.org/ontology/>
-        #     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        #     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-        #     SELECT *
-        #     WHERE {{
-        #     ?s1 dbp:symbol ?symbol .
-        #     ?s1 rdf:type ?exchange .
-        #     ?s1 rdfs:comment ?comment .
-        #     ?symbol bif:contains  '"{}"'  .
-        #     FILTER(lang(?comment) = "en").
-        #     FILTER(regex(str(?exchange), "{}" ) )
-        #     }}
-        # """.format(symbol, exch)
-        query.format(symbols)
+        query = queryFormatter(keyword, symbols)
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
@@ -63,11 +49,12 @@ class Queryer:
         try:
             if results['results']['bindings']:
                 results_df = pd.io.json.json_normalize(results['results']['bindings'])
-                results_df['exchangeLabel.value'] = results_df['exchangeLabel.value'].str.replace("New York Stock Exchange","NYSE")
-                results_df = results_df[['symbol.value','exchangeLabel.value','itemDescription.value']]
+                results_df['exchangeLabel.value'] = results_df['exchangeLabel.value'].str.replace("New York Stock Exchange","NYSE")            
+                results_df = results_df[['symbol.value','exchangeLabel.value','title.value','itemDescription.value', 'item.value']] if keyword is 'standard' else results_df[['exchangeLabel.value','title.value','itemDescription.value', 'item.value']]  
                 results_df = results_df.rename(columns={
                     'symbol.value':'symbol', 
                     'exchangeLabel.value':'exchange', 
+                    'title.value': 'title',
                     'itemDescription.value': 'wdDescription',
                     'item.value':'wdEntity'
                 })
@@ -75,71 +62,76 @@ class Queryer:
         return results_df
 
     def query_loop(self, df: pd.DataFrame, query: str):
-        symbols = ['^'+s+'$' for s in df.symbol.tolist()]
+        if query is 'standard':
+            symbols = ['^'+s+'$' for s in df.symbol.tolist()]
+        elif query is 'by_name':
+            symbols = [s.strip() for s in df.title.tolist()]
+            symbols = [re.sub(' +', ' ', s) for s in symbols]
         symbols_split = [list(c) for c in mit.divide(8, symbols)]
+
         results = pd.DataFrame()
         for i, s in enumerate(symbols_split):
             print("Parsing symbol list of size: {}, List: {}/{}".format(len(s), i+1, len(symbols_split)))
             symbols = '|'.join(s)
-            query_results = self.run_query(symbols, QUERIES['standard'])
+            query_results = self.run_query(symbols, query)
             results = results.append(query_results)
         print("Size of results returned: {}".format(results.shape[0]))
         return results
 
-    def find_missing_tags(self, diff: pd.DataFrame):
-        regex_pat = re.compile(r'Incorporated|Corporation|Company|Inc Common Stock|QQQ|ETF|PLC|SA|Inc|Corp|Ltd|LP|Co', flags=re.IGNORECASE)
-        diff['title'] = diff['title'].str.replace('[^\w\s]','')
-        diff['title'] = diff['title'].str.strip()
-        diff['title'] = diff['title'].str.replace(regex_pat, '')
-        print("Remaining symbols: {}".format(diff.shape[0]))
-        results = self.query_loop(diff, QUERIES['by_name'])
+    def fuzzy_string_comparison(self, df: pd.DataFrame, df_main: pd.DataFrame):
+        cashtags_main = df_main['title'].tolist()
+        cashtags_main = [s.lower() for s in cashtags_main]
+        for _, row in df.iterrows():
+            title = row['title'].lower()
+            ratios = {}
+            for tag in cashtags_main:
+                lev = _levenshtein.ratio(tag, title)
+                ratios[tag] = lev
+            matched_string = max(ratios, key=lambda key: ratios[key])
+            print("Title in query DF: {}, Title in original DF: {}".format(title, matched_string))
+            time.sleep(2)
+        sys.exit(0)
+
+
+    def clean_results(self, df: pd.DataFrame, keyword: str, df_main: pd.DataFrame=None):
+        df = df.copy()
+        df = df[df.exchange.str.contains('^NASDAQ$|^NYSE$')]
+        regex_companies = re.compile(r'(?:\s+(?:Incorporated|Corporation|Company|Inc Common Stock|QQQ|ETF|PLC|SA|Inc|Corp|Ltd|LP|plc|Group|The|Co|Limited))+\s*$', flags=re.IGNORECASE)
+        df['title'] = df['title'].str.replace('[^\w\s]','')
+        df['title'] = df['title'].str.strip()
+        df['title'] = df['title'].str.replace(' +|The ', ' ', regex=True)
+        df['title'] = df['title'].str.replace(regex_companies, '')
+        df['title'] = df['title'].str.strip()
+        print("Size of results after cleaning exchanges: {}".format(df.shape[0]))
+        if keyword is 'by_name':
+            self.fuzzy_string_comparison(df, df_main)
+        return df
 
     def run(self):
         df = pd.read_csv(self._rpath+'tag_cat_cashtags_clean.csv', sep='\t')
-        df = df[df.exchange.str.contains('^NASDAQ$|^NYSE$')]
+        # df = df[df.exchange.str.contains('^NASDAQ$|^NYSE$')]
+        df = self.clean_results(df, 'standard')
         df = df[['id', 'title', 'target', 'symbol', 'exchange']]
         symbols = ['^'+s+'$' for s in df.symbol.tolist()]
         symbols = '|'.join(symbols)
         try:
-            results = pd.read_csv(self._rpath+'tag_cat_results.csv', sep='\t')
+            res_file = pd.read_csv(self._rpath+'tag_cat_results.csv', sep='\t')
         except FileNotFoundError:
-            # symbols = ['^'+s+'$' for s in df.symbol.tolist()]
-            # symbols_split = [list(c) for c in mit.divide(8, symbols)]
-            # results = pd.DataFrame()
-            # for i, s in enumerate(symbols_split):
-            #     print("Parsing symbol list of size: {}, List: {}/{}".format(len(s), i+1, len(symbols_split)))
-            #     symbols = '|'.join(s)
-            #     query_results = self.run_query(symbols, QUERIES['standard'])
-            #     results = results.append(query_results)
-            # print("Size of results returned: {}".format(results.shape[0]))
-            results = self.query_loop(df, QUERIES['standard'])
+            results = self.query_loop(df, 'standard')
+            results = self.clean_results(results, 'standard')
+            print("Size of results after cleaning: {}".format(results.shape[0]))
             results.to_csv(self._rpath+'tag_cat_results.csv', sep="\t", index=False)
-        # print(df.symbol.shape[0]-len(results['symbol'].tolist()))
-        diff = df[~df['symbol'].isin(results['symbol'].tolist())].copy()
-        self.find_missing_tags(diff)
-
-        #symbols = '|'.join(symbols)
-        #print("Number of cashtags {}".format(df.shape[0]))
-        # query_results = self.run_query(symbols)
-        # print(query_results)
-
-        # print("Number of entries before description processing: {}".format(df.shape[0]))
-        # df['description'] = ''
-        # c = 0
-        # for i, row in df.iterrows():
-        #     desc = self.run_query(row.symbol, row.exchange)
-        #     print(row, "\n", desc)
-        #     sys.exit(0)
-        #     if not desc:
-        #         print("Row: {}, Hit Count: {}".format(i, c))
-        #         continue
-        #     c += 1
-        #     print("Row: {}, Hit Count: {}".format(i, c))
-        #     df.at[i, 'description'] = desc
-        
-        # print(df.head())
-        # sys.exit(0)
-        # print("Number of entries after description processing: {}".format(df.shape[0]))
+            res_file = results
+        diff = df[~df['symbol'].isin(res_file['symbol'].tolist())].copy()
+        try:
+            by_name_file = pd.read_csv(self._rpath+'tag_cat_by_name_temp.csv', sep='\t')
+            diff = df[~df['symbol'].isin(by_name_file['symbol'].tolist())].copy()
+            #results = self.clean_results(by_name_file, 'by_name', diff)
+        except FileNotFoundError:
+            print("Remaining symbols: {}".format(diff.shape[0]))
+            results = self.query_loop(diff, 'by_name')
+            results = self.clean_results(results, 'standard')
+            results.to_csv(self._rpath+'tag_cat_by_name_temp.csv', sep='\t', index=False)
 
 if __name__ == "__main__":
     queryer = Queryer()
